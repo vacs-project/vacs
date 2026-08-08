b1631c08395869237900082567ca2446381933de
Luis Kreihsl <luis.kreihsl@gmx.at>
2026-08-08 16:59:31 +0200

    feat(vacs-protocol): add call update server message

--- vacs-protocol/src/ws/server/calls.rs
+++ vacs-protocol/src/ws/server/calls.rs
@@ -18,6 +18,27 @@ pub struct CallInvitation {
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
+pub struct CallUpdate {
+    pub call_id: CallId,
+    pub invited_participants: CallParticipants,
+    pub joined_participants: CallParticipants,
+}
+
+impl CallUpdate {
+    pub fn all_participants(
+        &self,
+    ) -> std::iter::Chain<
+        std::collections::hash_map::Iter<'_, ClientId, CallTarget>,
+        std::collections::hash_map::Iter<'_, ClientId, CallTarget>,
+    > {
+        self.invited_participants
+            .iter()
+            .chain(self.joined_participants.iter())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub enum CallCancelReason {
     AnsweredElsewhere(ClientId),
     CallerCancelled,
